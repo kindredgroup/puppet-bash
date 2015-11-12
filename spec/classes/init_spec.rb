3288e911fd35aecc2f642d4a6aea45385cd9460b
Martin Hovmoller <martin.hovmoller@UNIBET.com>
2015-11-12 15:40:33 +0100

    Added test for bashrc.

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -5,5 +5,12 @@ describe 'bash' do
     it { should compile.with_all_deps }
     it { should contain_package('bash') }
     it { should contain_package('bash-completion') }
+    it { should_not contain_file('etc_bashrc') }
   end
+  context 'with bashrc => /foobar' do
+    let :params do { :bashrc => '/foobar' } end
+    it { should compile.with_all_deps }
+    it { should contain_file('etc_bashrc') }
+  end
+
 end
